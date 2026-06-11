c32e61aff2481ff528d96e74730c9ffc3c774ae4
masza1 <maszahid1@gmail.com>
2026-06-11 15:03:15 +0700

    vite aliases

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,6 +11,20 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      '@/shared': path.resolve(__dirname, './src/shared'),
+      '@/domains': path.resolve(__dirname, './src/domains'),
+      '@/server': path.resolve(__dirname, './src/server'),
+      '@/hooks': path.resolve(__dirname, './src/hooks'),
+      '@/services': path.resolve(__dirname, './src/services'),
+      '@/store': path.resolve(__dirname, './src/store'),
+      '@/types': path.resolve(__dirname, './src/types'),
+      '@/utils': path.resolve(__dirname, './src/utils'),
+      '@/schemas': path.resolve(__dirname, './src/schemas'),
+      '@/components': path.resolve(__dirname, './src/components'),
+      '@/locales': path.resolve(__dirname, './src/locales'),
+      '@/lib': path.resolve(__dirname, './src/lib'),
+      '@/providers': path.resolve(__dirname, './src/providers'),
+      '@/test': path.resolve(__dirname, './src/test'),
     },
   },
   server: {
