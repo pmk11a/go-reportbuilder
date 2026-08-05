1f2597d99a2a281d888f744b40291cf62c7a6eee
masza1 <maszahid1@gmail.com>
2026-08-05 08:35:44 +0700

    feat: enable seedReports in database seeder execution

--- backend/internal/infrastructure/database/seeders/seeder.go
+++ backend/internal/infrastructure/database/seeders/seeder.go
@@ -12,7 +12,7 @@ func SeedDatabase(database *gorm.DB) {
 	log.Println("Checking for seeds...")
 	seedSuperAdmin(database)
 	seedDBMenu(database)
-	// seedReports(database)
+	seedReports(database)
 	seedDbBrowseConfigs(database)
 }
 
